16a289d6796c9c76e698ec691e723e729d0c27b9
Stanislav Kosorin <stanokosorin4@gmail.com>
2023-03-26 21:08:05 +0200

    contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,6 +1,8 @@
+import { Tooltip } from '@nextui-org/react';
 import { useTranslation } from 'next-export-i18n';
 import { NextSeo } from 'next-seo';
 import React from 'react';
+import { AiFillGithub, AiFillLinkedin, AiOutlineMail } from 'react-icons/ai';
 
 export default function Contact() {
   const { t } = useTranslation();
@@ -8,9 +10,35 @@ export default function Contact() {
   return (
     <>
       <NextSeo title={t('contact.title')} description={t('contact.description')} />
-      <div id="contact" className="w-full min-h-screen bg-black">
-        <h1>Contact</h1>
-        <p>This is the Contact section.</p>
+      <div
+        id="contact"
+        className="relative flex flex-row items-start top-44 justify-center w-full min-h-screen bg-black"
+      >
+        <Tooltip content={<span className="text-2xl">{t('contact.linkedin')}</span>}>
+          <a
+            className="mx-10 flex items-center justify-center"
+            href="https://www.linkedin.com/in/stanislav-kosorin-b88452170/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <AiFillLinkedin size="20em" />
+          </a>
+        </Tooltip>
+        <Tooltip content={<span className="text-2xl">{t('contact.github')}</span>}>
+          <a
+            className="mx-10 flex items-center justify-center"
+            href="https://github.com/stano45"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <AiFillGithub size="20em" />
+          </a>
+        </Tooltip>
+        <Tooltip content={<span className="text-2xl">{t('contact.email')}</span>}>
+          <a className="mx-10 flex items-center justify-center" href="mailto:stanokosorin4@gmail.com">
+            <AiOutlineMail size="20em" />
+          </a>
+        </Tooltip>
       </div>
     </>
   );
