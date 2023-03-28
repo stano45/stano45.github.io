0ef361f71dda1b466f5641f7440a817e2f370215
Stanislav Kosorin <stanokosorin4@gmail.com>
2023-03-28 22:22:54 +0200

    last change today I swear

--- src/components/BlogPost.tsx
+++ src/components/BlogPost.tsx
@@ -3,7 +3,7 @@ import { BlogPostProps } from './types';
 export default function BlogPost({ post, children }: BlogPostProps) {
   return (
     <div className="max-w-4xl mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-4">{post.title}</h1>
+      <h1 className="text-3xl font-bold my-4">{post.title}</h1>
 
       <div className="flex justify-between items-center mb-4">
         <span className="text-gray-500">{post.date}</span>
