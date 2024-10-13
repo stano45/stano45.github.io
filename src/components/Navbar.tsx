70022f3612601deb7141ec4b151c62b6daec5933
Stanislav Kosorin <stanokosorin4@gmail.com>
2024-10-13 20:20:39 +0200

    Already pretty good

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,46 +32,26 @@ export function Navbar(): JSX.Element {
 					</span>
 				</Link>
 			</div>
-			<div className="lg:hidden">
-				<button
-					type="button"
-					onClick={toggleMenu}
-					className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
-				>
-					<svg
-						className="fill-current h-3 w-3"
-						viewBox="0 0 20 20"
-						xmlns="http://www.w3.org/2000/svg"
+			<div className="flex flex-row md:flex-grow items-center w-full md:flex md:items-center md:w-auto mt-3 md:mt-1">
+				{navbarOptions.map((option, idx) => (
+					<Link
+						key={idx.toString()}
+						href={{ pathname: option.path, query: query }}
 					>
-						<title>Menu</title>
-						<path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-					</svg>
-				</button>
-			</div>
-			<div
-				className={`${isMenuOpen ? "flex" : "hidden"} w-full text-lg lg:flex-grow lg:flex lg:items-center lg:w-auto`}
-			>
-				<div className="lg:flex-grow">
-					{navbarOptions.map((option, idx) => (
-						<Link
-							key={idx.toString()}
-							href={{ pathname: option.path, query: query }}
+						<button
+							type="button"
+							onClick={toggleMenu}
+							className={`block md:inline-block md:mt-0 ${
+								router.asPath.startsWith(option.path)
+									? "text-white"
+									: "text-pink-200"
+							} hover:text-white mr-6`}
 						>
-							<button
-								type="button"
-								onClick={toggleMenu}
-								className={`block mt-4 lg:inline-block  lg:mt-0 ${
-									router.asPath.startsWith(option.path)
-										? "text-white"
-										: "text-pink-200"
-								}  hover:text-white mr-6`}
-							>
-								{option.title}
-							</button>
-						</Link>
-					))}
-				</div>
-				<div className={isMenuOpen ? "self-end" : undefined}>
+							{option.title}
+						</button>
+					</Link>
+				))}
+				<div className="flex md:ml-auto">
 					<LanguagePicker />
 				</div>
 			</div>
