dc1aa9eda642d027cf73ea694432e37ae212caf9
Stanislav Kosorin <stanokosorin4@gmail.com>
2024-10-13 20:30:02 +0200

    Fix navbar on phones (#89)
    * Already pretty good
    
    * Improve on phone

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useTranslation, useLanguageQuery } from "next-export-i18n";
-import React, { useState } from "react";
+import React from "react";
 
 import { LanguagePicker } from "./LanguagePicker";
 
@@ -11,7 +11,6 @@ export function Navbar(): JSX.Element {
 	const { t } = useTranslation();
 	const [query] = useLanguageQuery();
 	const router = useRouter();
-	const [isMenuOpen, setIsMenuOpen] = useState(false);
 
 	const navbarOptions = [
 		{ title: t("blog.title"), path: "/blog" },
@@ -19,59 +18,34 @@ export function Navbar(): JSX.Element {
 		{ title: t("contact.title"), path: "/contact" },
 	];
 
-	const toggleMenu = () => {
-		setIsMenuOpen(!isMenuOpen);
-	};
-
 	return (
-		<nav className="flex items-center justify-between flex-wrap bg-blue-700 p-4">
-			<div className="flex items-center flex-shrink-0 text-white mr-6">
+		<nav className="flex flex-col md:flex-row items-center justify-center md:justify-between flex-wrap bg-blue-700 p-4">
+			<div className="flex items-center flex-shrink-0 text-white md:mx-6">
 				<Link href={{ pathname: "/", query: query }}>
 					<span className="font-semibold text-3xl tracking-tight">
 						Stanislav Kosorin
 					</span>
 				</Link>
 			</div>
-			<div className="lg:hidden">
-				<button
-					type="button"
-					onClick={toggleMenu}
-					className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
-				>
-					<svg
-						className="fill-current h-3 w-3"
-						viewBox="0 0 20 20"
-						xmlns="http://www.w3.org/2000/svg"
+			<div className="flex justify-center mt-3 md:flex-grow items-center w-full md:items-center md:w-auto  md:mt-1">
+				{navbarOptions.map((option, idx) => (
+					<Link
+						key={idx.toString()}
+						href={{ pathname: option.path, query: query }}
 					>
-						<title>Menu</title>
-						<path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-					</svg>
-				</button>
-			</div>
-			<div
-				className={`${isMenuOpen ? "flex" : "hidden"} w-full text-lg lg:flex-grow lg:flex lg:items-center lg:w-auto`}
-			>
-				<div className="lg:flex-grow">
-					{navbarOptions.map((option, idx) => (
-						<Link
-							key={idx.toString()}
-							href={{ pathname: option.path, query: query }}
+						<button
+							type="button"
+							className={`block md:inline-block md:mt-0 ${
+								router.asPath.startsWith(option.path)
+									? "text-white"
+									: "text-pink-200"
+							} hover:text-white mr-6`}
 						>
-							<button
-								type="button"
-								onClick={toggleMenu}
-								className={`block mt-4 lg:inline-block  lg:mt-0 ${
-									router.asPath.startsWith(option.path)
-										? "text-white"
-										: "text-pink-200"
-								}  hover:text-white mr-6`}
-							>
-								{option.title}
-							</button>
-						</Link>
-					))}
-				</div>
-				<div className={isMenuOpen ? "self-end" : undefined}>
+							{option.title}
+						</button>
+					</Link>
+				))}
+				<div className="flex md:ml-auto">
 					<LanguagePicker />
 				</div>
 			</div>
