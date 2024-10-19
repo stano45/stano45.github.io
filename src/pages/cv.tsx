12dd94c2a2d540014d51b4a597973efe11f53de6
Stanislav Kosorin <stanokosorin4@gmail.com>
2024-10-19 21:31:12 +0200

    Add cv pdf (#97)

--- src/pages/cv.tsx
+++ src/pages/cv.tsx
@@ -3,6 +3,8 @@ import { NextSeo } from "next-seo";
 import { Fragment, ReactElement } from "react";
 
 import { Period, type PeriodProps } from "@/components/Period";
+import { AiOutlineDownload } from "react-icons/ai";
+import { Tooltip } from "@nextui-org/react";
 
 export default function CV(): ReactElement {
 	const { t } = useTranslation();
@@ -11,6 +13,23 @@ export default function CV(): ReactElement {
 	return (
 		<div>
 			<NextSeo title={t("cv.title")} description={t("cv.description")} />
+			<div className="flex justify-between items-center mb-8">
+				<h1 className="text-2xl md:text-4xl font-bold text-center md:text-left  leading-relaxed ">
+					{t("cv.title")}
+				</h1>
+				<Tooltip
+					content={<span className="text-xl">{t("cv.download")}</span>}
+					offset={-5}
+				>
+					<a
+						className="opacity-0 fade-in-second"
+						href="/stanislav_kosorin_cv.pdf"
+						download="stanislav_kosorin_cv.pdf"
+					>
+						<AiOutlineDownload className="text-2xl md:text-4xl" />
+					</a>
+				</Tooltip>
+			</div>
 			{cvContent.map((period, index) => (
 				<Fragment key={period.title}>
 					<Period {...period} />
