d8dcc101b1d6a0fa3230df5083ba8314fc507c24
Stanislav Kosorin <stanokosorin4@gmail.com>
2025-02-17 20:10:42 +0100

    Add google tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-
+import { GoogleTagManager } from "@next/third-parties/google";
 import { Layout } from "@/components";
 import { ReactElement } from "react";
 
@@ -14,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps): ReactElement {
 					content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0"
 				/>
 			</Head>
+			<GoogleTagManager gtmId="GTM-T6DQN3W2" />
 			<Layout>
 				<Component {...pageProps} />
 			</Layout>
