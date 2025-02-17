df6ca6d3fed1cab37d577fe37fdd587068071a14
Stanislav Kosorin <stanokosorin4@gmail.com>
2025-02-17 23:03:54 +0100

    Add footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,4 @@
 import { ReactElement, type ReactNode } from "react";
-
 import { Navbar } from "./Navbar";
 
 interface LayoutProps {
@@ -8,13 +7,18 @@ interface LayoutProps {
 
 export function Layout({ children }: LayoutProps): ReactElement {
 	return (
-		<>
+		<div className="flex flex-col min-h-screen">
 			<Navbar />
-			<div className="flex justify-center">
+			<main className="flex-grow flex justify-center">
 				<div className="w-full h-full flex flex-col justify-center items-center md:w-1/2 m-4 md:mt-10 opacity-0 fade-in-first">
 					{children}
 				</div>
-			</div>
-		</>
+			</main>
+			<footer className="text-gray-600 text-center py-4">
+				<p>
+					© {new Date().getFullYear()} Stanislav Kosorin. All rights reserved.
+				</p>
+			</footer>
+		</div>
 	);
 }
