9defc28483b92d20f2e1dde06315a775010a8e2f
Stanislav Kosorin <stanokosorin4@gmail.com>
2025-09-07 12:57:02 +0200

    Add links to open resume, change tooltip strings

--- src/pages/education.tsx
+++ src/pages/education.tsx
@@ -1,7 +1,7 @@
 import { ReactElement } from "react";
 import { useTranslation } from "next-export-i18n";
 import { NextSeo } from "next-seo";
-import { AiOutlineDownload } from "react-icons/ai";
+import { AiOutlineDownload, AiOutlineFilePdf } from "react-icons/ai";
 import { Tooltip } from "@heroui/tooltip";
 
 import { type PeriodProps } from "@/components/Period";
@@ -18,22 +18,38 @@ export default function Education(): ReactElement {
 				description={t("education.description")}
 			/>
 			<div className="flex justify-between items-center mb-8">
-				<h1 className="text-2xl md:text-4xl font-bold text-center md:text-left leading-relaxed">
+				<h1 className="text-2xl md:text-4xl font-bold leading-relaxed">
 					{t("education.title")}
 				</h1>
-				<Tooltip
-					content={<span className="text-xl">{t("education.download")}</span>}
-					offset={-0.5}
-					closeDelay={150}
-				>
-					<a
-						className="transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 opacity-0 fade-in-second"
-						href="/stanislav_kosorin_education.pdf"
-						download="stanislav_kosorin_education.pdf"
+				<div className="flex gap-4">
+					<Tooltip
+						content={<span className="text-xl">{t("education.open")}</span>}
+						offset={-0.5}
+						closeDelay={150}
 					>
-						<AiOutlineDownload className="text-2xl md:text-4xl" />
-					</a>
-				</Tooltip>
+						<a
+							className="transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 opacity-0 fade-in-second"
+							href="Stanislav_Kosorin_Resume.pdf"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<AiOutlineFilePdf className="text-2xl md:text-4xl" />
+						</a>
+					</Tooltip>
+					<Tooltip
+						content={<span className="text-xl">{t("education.download")}</span>}
+						offset={-0.5}
+						closeDelay={150}
+					>
+						<a
+							className="transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 opacity-0 fade-in-second"
+							href="Stanislav_Kosorin_Resume.pdf"
+							download="Stanislav_Kosorin_Resume.pdf"
+						>
+							<AiOutlineDownload className="text-2xl md:text-4xl" />
+						</a>
+					</Tooltip>
+				</div>
 			</div>
 			<PeriodTimeline periods={periods} />
 		</div>
--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -1,7 +1,7 @@
 import { ReactElement } from "react";
 import { useTranslation } from "next-export-i18n";
 import { NextSeo } from "next-seo";
-import { AiOutlineDownload } from "react-icons/ai";
+import { AiOutlineDownload, AiOutlineFilePdf } from "react-icons/ai";
 import { Tooltip } from "@heroui/tooltip";
 
 import { type PeriodProps } from "@/components/Period";
@@ -15,22 +15,38 @@ export default function work(): ReactElement {
 		<div className="w-full h-full">
 			<NextSeo title={t("work.title")} description={t("work.description")} />
 			<div className="flex justify-between items-center mb-8">
-				<h1 className="text-2xl md:text-4xl font-bold text-center md:text-left leading-relaxed">
+				<h1 className="text-2xl md:text-4xl font-bold leading-relaxed">
 					{t("work.title")}
 				</h1>
-				<Tooltip
-					content={<span className="text-xl">{t("work.download")}</span>}
-					offset={-0.5}
-					closeDelay={150}
-				>
-					<a
-						className="transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 opacity-0 fade-in-second"
-						href="Stanislav_Kosorin_Resume.pdf"
-						download="Stanislav_Kosorin_Resume.pdf"
+				<div className="flex gap-4">
+					<Tooltip
+						content={<span className="text-xl">{t("work.open")}</span>}
+						offset={-0.5}
+						closeDelay={150}
 					>
-						<AiOutlineDownload className="text-2xl md:text-4xl" />
-					</a>
-				</Tooltip>
+						<a
+							className="transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 opacity-0 fade-in-second"
+							href="Stanislav_Kosorin_Resume.pdf"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<AiOutlineFilePdf className="text-2xl md:text-4xl" />
+						</a>
+					</Tooltip>
+					<Tooltip
+						content={<span className="text-xl">{t("work.download")}</span>}
+						offset={-0.5}
+						closeDelay={150}
+					>
+						<a
+							className="transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 opacity-0 fade-in-second"
+							href="Stanislav_Kosorin_Resume.pdf"
+							download="Stanislav_Kosorin_Resume.pdf"
+						>
+							<AiOutlineDownload className="text-2xl md:text-4xl" />
+						</a>
+					</Tooltip>
+				</div>
 			</div>
 			<PeriodTimeline periods={periods} />
 		</div>
